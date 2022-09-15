dff9f5d372e34a858dbd0ef3e1ed753b0ec5fc90
NeoThokala <97616080+NeoThokala@users.noreply.github.com>
2022-09-15 16:42:48 -0400

    Update numcount3.py

---
+++ mrlab/q3/numcount3.py
@@ -0,0 +1,21 @@
+from mrjob.job import MRJob   # MRJob version
+
+# Change the class name!!
+class WordCounter(MRJob):  #MRJob version
+    def mapper(self, key, line):
+     _txt=line.split()
+    _dict={}
+    for word in _txt:
+        x=0
+        for letter in word:
+               x++
+        if x > 2:
+        _dict[x]=_dict.get(x,0)+ 1
+    if _dict > 1:
+    return _dict
+
+    def reducer(self, key, values):
+        yield (key, sum(values))
+
+if __name__ == '__main__':
+    WordCounter.run()   # MRJob version
